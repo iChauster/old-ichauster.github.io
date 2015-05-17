d4772c71469ece3979f086ba921f91a57f036065
Ivan Chau <ivanmchau@gmail.com>
2015-05-17 19:19:06 -0400

    ht

--- js/contact_me.js
+++ js/contact_me.js
@@ -21,7 +21,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "http://ichauster.github.io/mail/contact_me.php",
+                url: "mail/contact_me.php",
                 type: "POST",
                 data: {
                     name: name,
